a09d2676664d7bf9d9f1cc38517cfb91af26aa24
sandrita <sandra@Sandras-Air.attlocal.net>
2021-05-05 21:59:22 -0700

    Before restart

--- js/app.js
+++ js/app.js
@@ -13,9 +13,43 @@
  * 
 */
 
+// /*define global variables*/
+// const navBarMenu = document.getElementById('navbar__list');
+// const components = document.querySelectorAll('section');
+// debugger;
+// /*list of item*/
+// let numberOfItems = components.length;
+
+// /*add elements to the navbar*/
+// for (let i=0; i<numberOfItems; i++) { 
+    
+//     let componentId = components[i].getAttribute('id')
+//     let componentName = components[i].getAttribute('data-nav')
+
+//     let allTogether = document.createElement('li')
+
+//     allTogether.innerHTML = `<a class='menu__link' href='#${componentId}'>${componentName}</a>`
+//     navBarMenu.appendChild(allTogether);
+// }
+
+// for (let i=0; i<numberOfItems; i++){
+    
+//     let componentId = components[i].getAttribute('id')
+
+//     let position = document.getElementById(componentId).getBoundingClientRect().top;
+//      if (position < 150 && position > -150) {
+//         components[i].classList.add('section.your-active-class');
+//     }
+//     else {
+//         components[i].classList.remove('section.your-active-class');
+//     }
+// }
+
+
+
 /*define global variables*/
-const navBarMenu = document.getElementById ('navbar__list');
-const components = document.querySelectorAll ('section');
+const navBarMenu = document.getElementById('navbar__list');
+const components = document.querySelectorAll('section');
 
 /*list of item*/
 let numberOfItems = components.length;
@@ -27,7 +61,20 @@ for (let i=0; i<numberOfItems; i++) {
     let componentName = components[i].getAttribute('data-nav')
 
     let allTogether = document.createElement('li')
-    allTogether.innerHTML = `<a class='menu__link' href='#${componentId}'>${componentName}</a>`
 
+    allTogether.innerHTML = `<a class='menu__link' href='#${componentId}'>${componentName}</a>`
     navBarMenu.appendChild(allTogether);
+}
+
+for (let i=0; i<numberOfItems; i++){
+    
+    let componentId = components[i].getAttribute('id')
+
+    let position = document.getElementById(componentId).getBoundingClientRect().top;
+     if (position < 150 && position > -150) {
+        components[i].classList.add('section.your-active-class');
+    }
+    else {
+        components[i].classList.remove('section.your-active-class');
+    }
 }
